a2e987cea1313a24a04bb43bad30df87a6563201
Leshchik_Vadim <vleshchik@gmail.com>
2023-06-29 13:41:47 +0300

    первая версия программы

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import numpy as np
 
 width = 100
 height = 100
-video = cv2.VideoWriter('running_text.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
+duration = 3
+
+video = cv2.VideoWriter('text.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
 background = np.zeros((height, width, 3), dtype=np.uint8)
 background.fill(255)
 text = input('Введите текст: ')
-font = cv2.FONT_HERSHEY_SIMPLEX
+font = cv2.FONT_HERSHEY_COMPLEX
 font_scale = 1
 font_color = (0, 0, 0)
 text_size, _ = cv2.getTextSize(text, font, font_scale, 1)
 x = width
-speed = 3
+
+speed = int(text_size[0] / duration)
 
 while x > -text_size[0]:
     frame = background.copy()
     cv2.putText(frame, text, (x, int(height / 2)), font, font_scale, font_color, 2)
     video.write(frame)
     x -= speed
+
 
 video.release()
--- test.py
+++ test.py
@@ -1,1 +1,23 @@
+#идея для добавления в базу данных:
+import psycopg2
 
+def save_query(text):
+    conn = psycopg2.connect(
+        host="your_host",
+        port="your_port",
+        database="your_database",
+        user="your_username",
+        password="your_password"
+    )
+
+    cursor = conn.cursor()
+    query = "INSERT INTO queries (text) VALUES (%s)"
+    cursor.execute(query, (text,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+
+text = "текст"
+save_query(text)
