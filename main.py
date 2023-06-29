79ba71de9efc61b2b49f28e05017dc8c1d6a8746
Leshchik_Vadim <vleshchik@gmail.com>
2023-06-29 12:34:26 +0300

    первая версия программы

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+width = 100
+height = 100
+video = cv2.VideoWriter('running_text.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
+background = np.zeros((height, width, 3), dtype=np.uint8)
+background.fill(255)
+text = input('Введите текст: ')
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
+font_color = (0, 0, 0)
+text_size, _ = cv2.getTextSize(text, font, font_scale, 1)
+x = width
+speed = 3
+
+while x > -text_size[0]:
+    frame = background.copy()
+    cv2.putText(frame, text, (x, int(height / 2)), font, font_scale, font_color, 2)
+    video.write(frame)
+    x -= speed
+
+video.release()
